e0afeb51afc62d5c7ef71359fd9195b3d10b580b
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 00:41:31 -0700

    quicksort.c printArray added spacing for better readability, and fixed % at the end of the line

--- quickSort.c
+++ quickSort.c
@@ -10,8 +10,9 @@ int partition(int arr[], int low, int high);
 
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d", arr[i]);
+        printf("%d ", arr[i]);
     }   
+    printf("\n");
 }
 
 int main() {
