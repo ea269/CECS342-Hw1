1588969fbdca5721cc040a2643a642fdd274bf15
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 00:19:04 -0700

    quicksort printArray functoin added

--- quickSort.c
+++ quickSort.c
@@ -2,12 +2,16 @@
 
 void swap(int *x, int *y);
 
-void quicksort(int array[], int length);
+void quicksort(int arr[], int length);
 
-void quicksortRecursion(int array[], int low, int high);
+void quicksortRecursion(int arr[], int low, int high);
 
-int partition(int, array[], int low, int high);
+int partition(int arr[], int low, int high);
+
+void printArray(in arr[], int size);
 
 int main() {
+
+
     return 0;
 }
