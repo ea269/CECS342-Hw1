c5d6eb9ee4e5ce7a661b415a3828fb66f3148611
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 00:14:17 -0700

    quicksort basic functions added

---
+++ quickSort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+
+void quicksort(int array[], int length);
+
+void quicksortRecursion(int array[], int low, int high);
+
+int partition(int, array[], int low, int high);
+
+int main() {
+    return 0;
+}
