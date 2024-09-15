c27a558e6576007753394fcc047d1c2309e05034
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 22:39:24 -0700

    updated quicksort.h file

--- quickSort.h
+++ quickSort.h
@@ -3,4 +3,10 @@
 
 void quick_sort(int *a, int n);
 
+void quicksortRecursion(int arr[], int low, int high);
+
+void swap(int *x, int *y);
+
+int partition(int arr[], int low, int high);
+
 #endif // QUICKSORT_H
