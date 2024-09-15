1d728ecea1a0a8c2378e37ee5f079570c37f8001
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-15 00:27:45 -0700

    Implemented randomArrayGenerator main

--- randomArrayGenerator.c
+++ randomArrayGenerator.c
@@ -24,6 +24,32 @@ void generateRandomArray(int low, int high, int count) {
 int main(int argc, char *argv[]) {
     // argc: num of command args passed in
     // argv: array of strings that contain the actual command args
+
+    if (argc != 4) { // if args are not 4 the user did not input one of the followings below
+        printf("%s <low> <high> <count>\n", argv[0]); // arg here would be the exec
+        printf("Arguments:\n");
+        printf("  <low>   : Lowest possible number in the array\n");
+        printf("  <high>  : Highest possible number in the array\n");
+        printf("  <count> : Number of elements in the array\n");
+        exit(1); // Exit error status
+    }
+    
+    // parsing in argv
+    int low = atoi(argv[1]);  
+    int high = atoi(argv[2]);
+    int count = atoi(argv[3]);
+    
+    if (low > high) { // basic conditions
+        printf("Error: The low value cannot be greater than the high value.\n");
+        return 1;
+    }
+
+    srand(time(NULL)); // seed the rand
+
+    generateRandomArray(low, high, count);
+
+    printf("Random array generated in randomArray.txt\n"); // print to know if successful
+
     return 0;
 }
 
