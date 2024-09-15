8651e22f378fcbc4142033ca94b95452bbf4112b
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 23:46:11 -0700

    Implemnted random logic

--- randomArrayGenerator.c
+++ randomArrayGenerator.c
@@ -6,6 +6,19 @@ void generateRandomArray(int low, int high, int count) {
     // low: lowest number possible in the array
     // high: highest number possible in the array
     // count: total number of elements in the array
+    FILE *file = fopen("randomArray.txt", "w"); // opens and writes into txt file
+    
+    if (file == NULL) { // checks if the file can be opened 
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < count; i++) { // generates random array
+        int randomValue = (rand() % (high - low + 1)) + low;
+        fprintf(file, "%d ", randomValue);
+    }
+
+    fclose(file); // closes file
 }
 
 int main(int argc, char *argv[]) {
