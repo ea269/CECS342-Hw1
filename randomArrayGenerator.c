262f77b9fe2522129999a49af316c4c905b6f4d0
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-14 23:22:01 -0700

    Attempting a random array genorator to make our arrays and study them

---
+++ randomArrayGenerator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateRandomArray(int low, int high, int count) {
+    // low: lowest number possible in the array
+    // high: highest number possible in the array
+    // count: total number of elements in the array
+}
+
+int main(int argc, char *argv[]) {
+    // argc: num of command args passed in
+    // argv: array of strings that contain the actual command args
+    return 0;
+}
+
