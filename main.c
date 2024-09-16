2efc2737eda4802d39f95bc2176905bb06363072
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-15 19:25:12 -0700

    Deleted readfromarray fun

--- main.c
+++ main.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include "quickSort.h"
 
-void readArrayFromFile(const char *filename, int arr[], int *size) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) { // condition if file cannot be open 
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    int number;
-    *size = 0;
-
-    while (fscanf(file, "%d", &number) == 1 && *size < MAX_SIZE) {
-        arr[*size] = number;
-        (*size)++;
-    }
-
-    fclose(file);
-}
-
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]); // %d prints int as decimal
