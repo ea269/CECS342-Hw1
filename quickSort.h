ab9851f5f1572b2abebc2e2d4869ef46266db343
ea269 <116852460+ea269@users.noreply.github.com>
2024-09-15 19:41:01 -0700

    Resolved conflict in quickSort.h during cherry-pick

---
+++ quickSort.h
@@ -0,0 +1,12 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+void quick_sort(int *a, int n);
+
+void quicksortRecursion(int arr[], int low, int high);
+
+void swap(int *x, int *y);
+
+int partition(int arr[], int low, int high);
+
+#endif 
