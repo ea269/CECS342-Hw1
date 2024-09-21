5ea09caebd20cf90e30f16818fbe6ba4646c2b8f
Admiral-Ham <ai.does.math1522gmail.com>
2024-09-20 19:57:56 -0700

    Added Main

--- quickSort.hs
+++ quickSort.hs
@@ -3,8 +3,15 @@
 
 qS :: Ord a=> [a] -> [a] 
 qS [] = []
-qS (x:xs) = qs head ++ x:qs tail 
+qS (x:xs) = qS head ++ x:qS tail 
     where 
         head = filter (<= x) xs
         tail = filter (> x) xs
 
+main = do
+    --empty_list []
+    putStr "Testing quicksort. \n qS With values = "
+    print(qS [5, -1, 2,1,-1,23, 1000, -9999])
+    putStr "Testing quicksort. \n qS with floating point nums = "
+    print(qS [0.05, 0.000125, 1.5, -3, 0])
+    
