44081a83d63e74c186dfb9f9146d3f4542f9dfa7
Admiral-Ham <ai.does.math1522gmail.com>
2024-09-20 20:34:11 -0700

    Spaces added

--- quickSort.hs
+++ quickSort.hs
@@ -3,7 +3,7 @@ qS :: Ord a => [a] -> [a]
 
 qS [] = []
 
-qS (x:xs) = qS head ++ x:qS tail 
+qS (x:xs) = qS head ++ x: qS tail 
     where 
         head = filter (<= x) xs
         tail = filter (> x) xs
@@ -11,12 +11,16 @@ qS (x:xs) = qS head ++ x:qS tail
 main = do
     putStr "\nTesting quickSort With values = "
     print(qS [5, -1, 2,1,-1,23, 1000, -9999])
+
     putStr "\nTesting quickSort with floating point nums = "
     print(qS [0.05, 0.000125, 1.5, -3, 0])
+    
     putStr "\nTesting quikSort with Char = "
     print(qS["1", "a", "c" , "as", " ", "300", "40k", "0", "A", "Z"])
+    
     putStr "\nTesting quickSort with [] = "
     print(qS ( [] :: [Int])) -- This is the only way I found to print and empty list using print
+    
     putStr "\nTesting quickSort with [nums] = "
     print(qS ( [1, 2, -1, 0] :: [Int])) --qS does order these in this fasion
     
