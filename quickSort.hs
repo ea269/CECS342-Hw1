3c04166235cd38375eb045769dc5654c9c96669a
Admiral-Ham <ai.does.math1522gmail.com>
2024-09-20 20:46:57 -0700

    function name change

--- quickSort.hs
+++ quickSort.hs
@@ -1,28 +1,28 @@
 --Quicksort
-qS :: Ord a => [a] -> [a] 
+qs :: Ord a => [a] -> [a] 
 
-qS [] = []
+qs [] = []
 
-qS (x:xs) = qS head ++ x: qS tail 
+qs (x:xs) = qs head ++ x: qs tail 
     where 
         head = filter (<= x) xs
         tail = filter (> x) xs
 
 main = do
     putStr "\nTesting quickSort With values = "
-    print(qS [5, -1, 2,1,-1,23, 1000, -9999])
+    print(qs [5, -1, 2,1,-1,23, 1000, -9999])
 
     putStr "\nTesting quickSort with floating point nums = "
-    print(qS [0.05, 0.000125, 1.5, -3, 0])
+    print(qs [0.05, 0.000125, 1.5, -3, 0])
     
     putStr "\nTesting quikSort with Char = "
-    print(qS["1", "a", "c" , "as", " ", "300", "40k", "0", "A", "Z"])
+    print(qs["1", "a", "c" , "as", " ", "300", "40k", "0", "A", "Z"])
     
     putStr "\nTesting quickSort with [] = "
-    print(qS ( [] :: [Int])) -- This is the only way I found to print and empty list using print
+    print(qs ( [] :: [Int])) -- This is the only way I found to print and empty list using print
     
     putStr "\nTesting quickSort with [nums] = "
-    print(qS ( [1, 2, -1, 0] :: [Int])) --qS does order these in this fasion
+    print(qs ( [1, 2, -1, 0] :: [Int])) --qs does order these in this fasion
     
-    --print(qS ([])) This does not work. It always results in an error
+    --print(qs ([])) This does not work. It always results in an error
     
