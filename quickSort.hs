8a08361dd1360bf3b0ced841c6f5f8325f933ca7
Admiral-Ham <ai.does.math1522gmail.com>
2024-09-20 20:31:11 -0700

    Added way to print empty list

--- quickSort.hs
+++ quickSort.hs
@@ -1,15 +1,14 @@
-
-
-
+--Quicksort
 qS :: Ord a => [a] -> [a] 
+
 qS [] = []
+
 qS (x:xs) = qS head ++ x:qS tail 
     where 
         head = filter (<= x) xs
         tail = filter (> x) xs
 
 main = do
-    --empty_list []
     putStr "\nTesting quickSort With values = "
     print(qS [5, -1, 2,1,-1,23, 1000, -9999])
     putStr "\nTesting quickSort with floating point nums = "
@@ -17,4 +16,9 @@ main = do
     putStr "\nTesting quikSort with Char = "
     print(qS["1", "a", "c" , "as", " ", "300", "40k", "0", "A", "Z"])
     putStr "\nTesting quickSort with [] = "
-    --print(qS [])
+    print(qS ( [] :: [Int])) -- This is the only way I found to print and empty list using print
+    putStr "\nTesting quickSort with [nums] = "
+    print(qS ( [1, 2, -1, 0] :: [Int])) --qS does order these in this fasion
+    
+    --print(qS ([])) This does not work. It always results in an error
+    
